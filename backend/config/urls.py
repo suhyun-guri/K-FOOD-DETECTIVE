b07988e71c29b777b956e64b3e4cd1ba1ca4e6ab
castlemin <csm1165@naver.com>
2022-02-26 16:07:20 +0900

    feat: routed 'account' app

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -13,11 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('chilsu/', include('chilsutest.urls')),
+    path('account/', include('account.urls')),
+    path('account/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('account/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
