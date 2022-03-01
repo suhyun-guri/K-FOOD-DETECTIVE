ef609a01d3f025ffc2d9d9b218543c5a091759e3
castlemin <csm1165@naver.com>
2022-03-01 23:16:55 +0900

    feat: modified User model and UserManager 1. changed DB table name 2. added field 'is staff' to make superuser 3. modified '_create_user' function to make superuser

--- backend/account/models.py
+++ backend/account/models.py
@@ -9,9 +9,9 @@ class CustomUserManager(BaseUserManager):
 
     use_in_migrations = True  
 
-    def _create_user(self, username, password, profile_image, email, nationality='please set this value', gender=4, age=7, **extra_fields):
+    def _create_user(self, username, password, email, **extra_fields):
         
-        values = [email, nationality, gender, age, profile_image]
+        values = [email]
 
         field_value_map = dict(zip(self.model.REQUIRED_FIELDS, values))
         for field_name, value in field_value_map.items():
@@ -22,29 +22,30 @@ class CustomUserManager(BaseUserManager):
         user = self.model(
             username=username,
             email=email,
-            nationality=nationality,
-            gender=gender,
-            age=age,
-            profile_image=profile_image,
             **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
+
         return user
 
-    def create_user(self, username, password, email, profile_image, nationality='please set this value', gender=4, age=7,  **extra_fields):
-        extra_fields.setdefault('is_superuser', False)
-        return self._create_user(username, password, email, nationality, gender, age, profile_image, **extra_fields)
+    def create_user(self, username, password, email, **extra_fields):
+        
+        return self._create_user(username, password, email, **extra_fields)
 
-    def create_superuser(self, username, password, profile_image, email="admin@admin.com", nationality='please set this value', gender=4, age=7, **extra_fields):
+    def create_superuser(self, username, password, email, **extra_fields):
         extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_staff', True)
 
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
 
-        return self._create_user(username, password, email, nationality, gender, age, profile_image, **extra_fields)
+        return self._create_user(username, password, email, **extra_fields)
 
+        
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
+    class Meta:
+        db_table = "user"
     
     class GenderChoice(models.IntegerChoices):
         male = 0, 'male'
@@ -70,6 +71,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     age = models.SmallIntegerField(choices=AgeChoice.choices, default=AgeChoice.empty, blank=True)
     profile_image = models.ImageField(upload_to=profile_directory_path, default='profile/default_img.jpg')
     is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
     date_joined = models.DateField(auto_now_add=True)
 
     objects = CustomUserManager()
