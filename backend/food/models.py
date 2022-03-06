48d8b573ef87305594e456edbd6e35d60415a7bc
castlemin <csm1165@naver.com>
2022-03-06 12:10:52 +0900

    feat: added models for food API

--- backend/food/models.py
+++ backend/food/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from account.models import CustomUser
 
-# Create your models here.
+
+class Food(models.Model):
+    class Meta:
+        db_table = 'food'
+    
+    korean_name = models.CharField(unique=True, max_length=255)
+    english_name = models.CharField(unique=True, max_length=255)
+    romanized_name = models.CharField(unique=True, max_length=255)
+    hit = models.IntegerField(default=0)
+    image_url = models.URLField(max_length=500)
+    info = models.CharField(max_length=1000)
+    scrap_users = models.ManyToManyField(CustomUser, related_name='scrap_foods', db_table='scrap') 
+
+
+class Recipe(models.Model):
+    class Meta:
+        db_table = 'recipe'
+    
+    food = models.ForeignKey(Food, on_delete=models.CASCADE, related_name='recipes')
+    url = models.URLField(max_length=500)
+
+class Category(models.Model):
+    class Meta:
+        db_table = 'category'
+    
+    foods = models.ManyToManyField(Food, related_name='categories')
+    name = models.CharField(unique=True, max_length=255)
+
+class Ingredient(models.Model):
+    class Meta:
+        db_table = 'ingredient'
+    
+    foods = models.ManyToManyField(Food, related_name='ingredients')
+    name = models.CharField(unique=True, max_length=255)
+
+class Taste(models.Model):
+    class Meta:
+        db_table = 'taste'
+    
+    food = models.ForeignKey(Food, on_delete=models.CASCADE, related_name='tastes')
+    oily = models.SmallIntegerField()
+    spicy = models.SmallIntegerField()
+    sour = models.SmallIntegerField()
+    salty = models.SmallIntegerField()
