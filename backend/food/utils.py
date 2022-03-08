d1c35d9af3e8a4970802e9b960ef58c2d5f7ebb9
castlemin <csm1165@naver.com>
2022-03-08 16:36:54 +0900

    refactor: changed variable names

--- backend/food/utils.py
+++ backend/food/utils.py
@@ -2,6 +2,7 @@ import numpy as np
 from numpy.linalg import norm
 import pandas as pd
 
+NUM_OF_RESULT = 2
 
 #코사인 유사도 계산 함수 
 def cos_sim(A, B):
@@ -9,9 +10,9 @@ def cos_sim(A, B):
 
 #main function
 def recommender_system(df, user_score=list):
-    df1 = df.copy()
+    food_dataframe = df.copy()
     result = []
-    for i in range(df1.shape[0]):
-        result.append(cos_sim(df1.iloc[i,1:].tolist(), user_score))
-    df1['cos_sim'] = result
-    return df1.sort_values('cos_sim', ascending=False)['romanized_name'][:2].tolist()
+    for i in range(food_dataframe.shape[0]):
+        result.append(cos_sim(food_dataframe.iloc[i,1:].tolist(), user_score))
+    food_dataframe['cos_sim'] = result
+    return food_dataframe.sort_values('cos_sim', ascending=False)['romanized_name'][:NUM_OF_RESULT].tolist()
